e5143cbb151bd92f8c75d401ebda5fc6118fa9e4
SamuelBurgers <sambburgers@gmail.com>
2024-09-27 18:43:35 +0200

    Initial commit

---
+++ Java.js
@@ -0,0 +1,39 @@
+
+    //piet(1);
+    // piet(2);
+    // piet(3);
+    // let licht=0;
+    // function piet(x) {
+    //     let (x==1) = ["red", "orange", "green"]
+    //     document.getElementById("licht").style.backgroundColor = a[licht];
+    //     licht++;
+    //     //if(licht==2) licht=0;
+    //     licht = licht % 3;
+    // }
+    //for(let x=s;x<n;x++) {
+    //    console.log(x);
+    // if (x==1) {
+    //      console.log("bericht");
+    // } else {
+    //console.log("anders");
+    // }
+    //switch (x) {
+        //case 1:
+            //console.log("rood");
+            //document.getElementById("licht").style.backgroundColor = "red";
+            //break;
+       // case 2:
+            //console.log("oranje");
+           // break;
+        //case 3:
+            //console.log("groen");
+            //break;
+       // default:
+            //console.log("anders");
+           // break;
+       // }
+   // }
+//}
+
+
+// var nooit meer nu alleen nog maar "const" of "let";
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+//integer
+$age = 48 . "<br>";
+echo $age;
+
+$adult = 17;
+
+if ($adult >= 18) {
+    echo 'Adult <br>';
+} else {
+    echo 'child!!!!! <br>';
+}
+
+$grootgetal = PHP_INT_MAX;
+echo number_format($grootgetal) . " index.php" . gettype($grootgetal) . "<br>";
+
+$fruit = ['apple', 'banana', 'orange'];
+
+//for, foreach, (do) while,
+
+//foreach ($fruit as $fruit) {
+//    echo $fruit . "<br>";
+//}
+
+
+//for loop
+echo "<br><strong>for loop</strong><br>";
+
+for ($i = 0; $i < count($fruit); $i++) {
+    if ($fruit[$i] === 'banana' ) {continue;}
+        if ($i !=1){
+    echo $fruit[$i] . "<br>";
+}
+}
+
+//while loop
+echo "<br><strong>while loop</strong><br>";
+
+$i =0;
+
+while ($i <count($fruit)) {
+    echo $fruit[$i] . "<br>";
+    $i++;
+}
+
+echo "<br><strong>Hobbies foreach</strong><br>";
+
+$hobbies = ['Wandelen', 'Gamen', 'Boulderen', 'Gitaar spelen'];
+
+foreach ($hobbies as $hobby) {
+    echo $hobby . "<br>";
+}
+
+echo "<br><strong>calc</strong><br>";
+
+$i = 1;
+$u = 10;
+for ($i; $i <= 10; $i++) {
+    $result = $u * $i;{
+    echo "<p>". $i . "x" . $u . "=" . $result . "<br>" . "</p>" ;}
+}
+
+
+//met deze fruit array doen
